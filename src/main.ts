0836aea25b656b4d5ad0a956c7d7196d991a4be9
hasan rahhal <hasan.rahhall@gmail.com>
2020-09-21 00:44:51 +0200

    type added for app

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/utils/http-exception.filter';
+import { INestApplication } from '@nestjs/common';
 
-function addSwagger(app) {
+function addSwagger(app: INestApplication) {
   const options = new DocumentBuilder()
     .setTitle('Project Management Api')
     .setDescription('The Project Management API ')
@@ -15,8 +16,11 @@ function addSwagger(app) {
   SwaggerModule.setup('swagger', app, document);
 }
 
+/**
+ * Initialize the application + add swagger and middleware
+ */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
   addSwagger(app)
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
